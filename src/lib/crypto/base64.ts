6fb3f85bac63cc83c21ac584ebff9cb095dd3d91
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-08-25 15:11:06 +0200

    rm async from base64

--- src/lib/crypto/base64.ts
+++ src/lib/crypto/base64.ts
@@ -1,7 +1,7 @@
-export const encodeBase64 = async (payload: string) => {
+export const encodeBase64 = (payload: string) => {
   return Buffer.from(payload).toString('base64')
 }
 
-export const decodeBase64 = async (payload: string) => {
+export const decodeBase64 = (payload: string) => {
   return Buffer.from(payload, 'base64').toString('utf-8')
 }
